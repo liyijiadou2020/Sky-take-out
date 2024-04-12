ea2655a0f0cd096bc454b8996a1474184fcf4ebd
liyijia <liyijiadou@gmail.com>
2024-04-12 18:33:16 +0300

    菜品分页查询开发

--- demo/sky-take-out/sky-server/src/main/java/com/sky/service/DishService.java
+++ demo/sky-take-out/sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,5 +1,7 @@
 package com.sky.service;
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 /**
  * 菜品
  *
@@ -14,4 +16,11 @@ public interface DishService {
      */
     public void saveWithFlavor(DishDTO dishDTO);
 
+    /**
+     * 菜品分页查询
+     *
+     * @param dishPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
