885f2da54ceb70ee151a5532204e5cb7ff8aa9ee
liyijia <liyijiadou@gmail.com>
2024-04-18 12:14:25 +0300

    订单、销量排名数据统计代码开发

--- demo/sky-take-out/sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ demo/sky-take-out/sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -1,6 +1,8 @@
 package com.sky.controller.admin;
 import com.sky.result.Result;
 import com.sky.service.ReportService;
+import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import io.swagger.annotations.Api;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 /**
  * 数据统计相关接口
@@ -56,6 +59,47 @@ public class ReportController {
         log.info("用户数据统计：{} ~ {}", begin, end);
         return Result.success(reportService.getUserStatistics(begin,end));
     }
+
+
+    /**
+     * 订单统计
+     * @param begin
+     * @param end
+     * @return
+     */
+    @GetMapping("/ordersStatistics")
+    @ApiOperation("订单统计")
+    public Result<OrderReportVO> ordersStatistics(
+            @DateTimeFormat(pattern = "yyyy-MM-dd")  LocalDate begin,
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
+        log.info("订单数据统计：{},{}",begin,end);
+        return Result.success(reportService.getOrderStatistics(begin,end));
+    }
+
+    /**
+     * 销量排名top10
+     * @param begin
+     * @param end
+     * @return
+     */
+    @GetMapping("/top10")
+    @ApiOperation("销量排名top10")
+    public Result<SalesTop10ReportVO> top10(
+            @DateTimeFormat(pattern = "yyyy-MM-dd")  LocalDate begin,
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
+        log.info("销量排名top10：{},{}",begin,end);
+        return Result.success(reportService.getSalesTop10(begin,end));
+    }
+
+    /**
+     * 导出运营数据报表
+     * @param response
+     */
+    // @GetMapping("/export")
+    // @ApiOperation("导出运营数据报表")
+    // public void export(HttpServletResponse response){
+    //     reportService.exportBusinessData(response);
+    // }
 
 
 }
--- demo/sky-take-out/sky-server/src/main/java/com/sky/service/ReportService.java
+++ demo/sky-take-out/sky-server/src/main/java/com/sky/service/ReportService.java
@@ -1,7 +1,10 @@
 package com.sky.service;
+import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 /**
  * 统计营业额的相关业务
@@ -25,4 +28,26 @@ public interface ReportService {
      * @return
      */
     UserReportVO getUserStatistics(LocalDate begin, LocalDate end);
+
+    /**
+     * 统计指定时间区间内的订单数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    OrderReportVO getOrderStatistics(LocalDate begin, LocalDate end);
+
+    /**
+     * 统计指定时间区间内的销量排名前10
+     * @param begin
+     * @param end
+     * @return
+     */
+    SalesTop10ReportVO getSalesTop10(LocalDate begin, LocalDate end);
+
+    /**
+     * 导出运营数据报表
+     * @param response
+     */
+    // void exportBusinessData(HttpServletResponse response);
 }
